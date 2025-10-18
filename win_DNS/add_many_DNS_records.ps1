428f12cbd1547babc20b5ee60085bb4bdf6fcd13
poorjude <kuzminykh.v.m17@gmail.com>
2025-10-18 23:21:35 +0300

    Add win_DNS scripts

---
+++ win_DNS/add_many_DNS_records.ps1
@@ -0,0 +1,18 @@
+﻿$TTL = "00:20:00"
+$PrimaryZoneName = "domain.local"
+$MiddleZoneName = ".subdomain.msk" # Указываем вместе с точкой в начале (или оставляем полностью пустую строку)
+
+$allRecordsData = Get-Content -Path ".\add_many_DNS_records_input.csv" | ForEach-Object { 
+    [PSCustomObject]@{ 
+        IP = $_.Split(';')[0]
+        Hostname = $_.Split(';')[1] 
+    } 
+}
+
+foreach ($recordData in $allRecordsData ) {
+    Add-DnsServerResourceRecordA `
+      -ZoneName $PrimaryZoneName `
+      -Name ($recordData.Hostname + $MiddleZoneName) `
+      -IPv4Address $recordData.IP `
+      -TimeToLive $TTL
+}
---
+++ win_DNS/get_DNS_debug_logs_from_many_domains.ps1
@@ -0,0 +1,47 @@
+﻿# Указываем DNS-серверы
+$dns_servers = @(
+    "dc1.domain1.local"
+    "dc2.domain1.local"
+    "dc1.domain2.local"
+    "dc1.domain3.local"
+    "dc1.domain4.local"
+    "dc2.domain4.local"
+)
+
+# Вводим credentials для каждого домена
+$domain1_creds = Get-Credential -Message "Enter creds for domain1.local DNS-servers"
+$domain2_creds = Get-Credential -Message "Enter creds for domain2.local DNS-servers"
+$domain3_creds = Get-Credential -Message "Enter creds for domain3.local DNS-servers"
+$domain4_creds = Get-Credential -Message "Enter creds for domain4.local DNS-servers"
+
+# Создаём папку, куда будем копировать логи DNS
+$logFolderLocal = "C:\DNS_logs_" + (Get-Date -Format "dd.MM.yyyy_HH.mm.ss").ToString()
+New-Item -Path $logFolderLocal -ItemType Directory -Force
+
+foreach ($dns_server in $dns_servers) {
+    # Выбираем, какие credentials будем использовать
+    if ($dns_server -like "*.domain1.local") {
+        $creds = $domain1_creds
+    } elseif ($dns_server -like "*.domain2.local") {
+        $creds = $domain2_creds
+    } elseif ($dns_server -like "*.domain3.local") {
+        $creds = $domain3_creds
+    } elseif ($dns_server -like "*.domain4.local") {
+        $creds = $domain4_creds
+    } else {
+        "Для данного хоста не найдены подходящие креды: $dns_server"
+        continue
+    }
+
+    # Получаем месторасположение логов на DNS-сервере
+    $logFilePathRemote = Invoke-Command -ComputerName $dns_server -Credential $creds {
+        (Get-DnsServerDiagnostics).LogFilePath
+    }
+
+    $logFilePathLocal = $logFolderLocal + '\' + $dns_server + '.txt'
+
+    # Копируем логи
+    $session = New-PSSession -ComputerName $dns_server -Credential $creds
+    Copy-Item -Path $logFilePathRemote -Destination $logFilePathLocal -FromSession $session -Verbose
+    Remove-PSSession $session
+}
